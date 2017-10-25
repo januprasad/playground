17052097e39271f2fb7e3f17ab564ea630109755
Janu Prasad K <janu@hatio.co>
2017-10-25 14:27:36 +0530

    java app

--- app/src/main/java/playground/jenuine/MainActivity.java
+++ app/src/main/java/playground/jenuine/MainActivity.java
@@ -1,7 +1,9 @@
 package playground.jenuine;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,12 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    protected void openBrowser(View view){
+        Intent intent = new Intent();
+        intent.setAction(Intent.ACTION_VIEW);
+        intent.setData(android.net.Uri.parse("http://www.google.com"));
+        startActivity(intent);
     }
 }
